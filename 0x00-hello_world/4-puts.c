3bd409b39c3548262846eda416763e1b0cbdcbf5
ihebchatti <chatti.iheb93@gmail.com>
2020-02-05 13:54:49 +0100

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *main - Using puts to display a certain text with a new line
- */
+*main - Using puts to display a certain text with a new line
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
