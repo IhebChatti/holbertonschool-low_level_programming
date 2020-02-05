f52fdd75f31ddd5b3f5757546f92746045baa967
ihebchatti <chatti.iheb93@gmail.com>
2020-02-05 13:58:42 +0100

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
-int main (void)
+/**
+*main - Using printf to display a statement
+*Return: 0
+*/
+int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return 0;
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
