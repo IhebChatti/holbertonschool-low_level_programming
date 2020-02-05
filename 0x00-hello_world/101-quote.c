e0d329750b6405f56148030431bc390cf769150d
ihebchatti <chatti.iheb93@gmail.com>
2020-02-05 14:38:11 +0100

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
+#include <unistd.h>
 #include <stdio.h>
 /**
 *main - Using perror to display a statement
 *Return: 1
 */
-int main (void)
+int main(void)
 {
-perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-return(1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
