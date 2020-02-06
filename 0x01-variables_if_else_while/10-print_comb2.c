fe5ed3abdfbf6555523b45c971cf275292acd95c
ihebchatti <chatti.iheb93@gmail.com>
2020-02-06 15:57:43 +0100

    comb2

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - print all possible combinations of single-digit number
+*Return: 0
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 100; i++)
+{
+  if (i != 100)
+  putchar((i/10) + '0');
+  putchar ((i%10)+'0');
+  putchar (',');
+    putchar (' ');
+}
+putchar ('\n');
+return (0);
+}
