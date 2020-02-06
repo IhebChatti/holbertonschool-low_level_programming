689f7af12072fec60c75e3848d869e2a4cbcf9e8
ihebchatti <chatti.iheb93@gmail.com>
2020-02-06 11:35:02 +0100

    alphabetPUTCHAR

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - writing the alphabet using puts
+*Return: 0
+*/
+int main(void)
+{
+char c;
+c = 'a';
+while (c <= 'z')
+{
+putchar(c);
+putchar('\n');
+c++;
+}
+}
