fa1c09291702344cea2947a3f0cb186f109c0880
ihebchatti <chatti.iheb93@gmail.com>
2020-02-06 18:02:50 +0100

    comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - print all possible combinations of two-digits number
+*Return: 0
+*/
+int main(void)
+{
+  int  i, j, k;
+for (i = '0'; i <= '9'; i++)
+{
+  k=i;
+  k++;
+for (j = k; j <= '9'; j++)
+{
+putchar(i);
+putchar(j);
+if (!(i == '9' && j == '9'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar ('\n');
+return (0);
+}
