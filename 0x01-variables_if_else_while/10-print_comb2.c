7581475b09e9c8ab5bcbde90d496c8f4b6b15cff
ihebchatti <chatti.iheb93@gmail.com>
2020-02-06 16:01:07 +0100

    comb2

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -9,11 +9,10 @@ int main(void)
 int i;
 for (i = 0; i < 100; i++)
 {
-  if (i != 100)
-  putchar((i/10) + '0');
-  putchar ((i%10)+'0');
-  putchar (',');
-    putchar (' ');
+putchar ((i / 10) + '0');
+putchar ((i % 10) + '0');
+putchar (',');
+putchar (' ');
 }
 putchar ('\n');
 return (0);
