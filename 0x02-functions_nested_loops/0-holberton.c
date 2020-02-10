d891668abf395dbdd9c34a9c06a454c6f01773cf
ihebchatti <chatti.iheb93@gmail.com>
2020-02-10 16:15:29 +0100

    Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#define putchar(c)
 /**
 *main - Entry point
 *Return: 0
@@ -6,9 +7,11 @@
 int main(void)
 {
 	char *c = "Holberton";
+
 	while (*c)
 	{
-		putchar (*c++);
+		putchar (*c);
+		c++;
 	}
 	putchar ('\n');
 	return (0);
