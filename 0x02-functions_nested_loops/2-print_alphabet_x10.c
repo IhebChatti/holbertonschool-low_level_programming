6eef1a4d03978bf1adfbefe56e0d2b83532a8b8a
ihebchatti <chatti.iheb93@gmail.com>
2020-02-10 13:38:58 +0100

    alphabetX10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+*print_alphabet - prints the alphabet 10 times
+*Return: 0
+*/
+void print_alphabet(void)
+{
+	int i = 0;
+	char c = 'a';
+	while (i < 10)
+	{
+		while (c <= 'z')
+		{
+			putchar(c);
+			c++;
+		}
+		putchar('\n');
+		i++;
+	}
+	return (0);
+}
