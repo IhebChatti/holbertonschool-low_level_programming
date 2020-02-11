9947708c8ea8823844a743e26f3cb5976d68778b
ihebchatti <chatti.iheb93@gmail.com>
2020-02-11 23:53:50 +0100

    fib2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/*
+*main - Entry point
+*Return: 0
+*/
+int main(void)
+{
+	int i;
+	long x = 1, y = 2;
+
+	printf("%ld, %ld, ", x, y);
+	for (i = x + y; i <= 50; i++)
+	{
+		y = y + x;
+		x = y - x;
+		printf("%ld", y);
+		if (i != 50)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+	return (0);
+}
