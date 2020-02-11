bbc175f76d79efd741363cdfe96a30a9593e1bcf
ihebchatti <chatti.iheb93@gmail.com>
2020-02-11 13:41:21 +0100

    timesTable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,26 +1,30 @@
 #include "holberton.h"
 /**
 *times_table - displays the times table from 0 to 9
-*Return: 0
+*Return: void
 */
-int main(void)
+void times_table(void)
 {
-	int i, j;
-	int mul;
+	int i, j, mul;
 
 	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
-		{
-		mul	= i * j;
-		_putchar (mul + '0');
-		if (i <= 8 && j <= 8)
+		_putchar ('0');
+		for (j = 1; j <= 9; j++)
 		{
 			_putchar (',');
 			_putchar (' ');
+			mul	= i * j;
+		if (mul <= 9)
+		{
+			_putchar (' ');
+		}
+		else
+		{
+			_putchar ((mul / 10) + '0');
+			_putchar ((mul % 10) + '0');
 		}
 		}
-	_putchar ('\n');
+		_putchar ('\n');
 	}
-	return (0);
 }
