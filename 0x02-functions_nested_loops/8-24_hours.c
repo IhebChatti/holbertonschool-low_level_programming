5185150fdb1f76b116464e766568fc3ecf8b7c2d
ihebchatti <chatti.iheb93@gmail.com>
2020-02-11 10:10:45 +0100

    JackBauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,11 +10,11 @@ void jack_bauer(void)
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60; m++)
-			_putchar (h / 10);
 			_putchar (h % 10);
+			_putchar (h / 10);
 			_putchar (':');
-			_putchar (m / 10);
 			_putchar (m % 10);
+			_putchar (m / 10);
 			_putchar ('\n');
 	}
 }
