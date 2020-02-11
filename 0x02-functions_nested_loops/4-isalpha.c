e421ae053c739aaa996853610246bd15adf00a0b
ihebchatti <chatti.iheb93@gmail.com>
2020-02-11 10:51:52 +0100

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,10 +6,9 @@
 */
 int _isalpha(int c)
 {
-	int c;
-
-	if ((c > 'A' - 1 && c < 'Z' + 1) || (c > 'a' - 1 && c > 'z' + 1))
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
 	{
+
 		return (1);
 	}
 	else
