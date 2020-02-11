28a009400c4fb86a31b308604222c819ebf4a8e2
ihebchatti <chatti.iheb93@gmail.com>
2020-02-11 23:23:02 +0100

    nature

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,9 +7,9 @@ int main(void)
 {
 	int i, S = 23;
 
-	for (i = 10; i < 1024; i++)
+	for (i = 0; i < 1024; i++)
 	{
-		if (i % 5 == 0 || i % 3 == 0)
+		if 	(i % 5 == 0 || i % 3 == 0)
 		{
 			S = S + 1;
 		}
