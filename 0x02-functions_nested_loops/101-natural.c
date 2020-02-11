4b059df8aa7288e650a23d4fe85672755054367a
ihebchatti <chatti.iheb93@gmail.com>
2020-02-11 23:17:05 +0100

    Nature

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: 0
+*/
+int main(void)
+{
+	int i, S = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 5 == 0) || (i % 3 == 0))
+		{
+			S = S + 1;
+		}
+	}
+	printf("%d\n", S);
+	return (0);
+}
