ff8bdad3d923b3dd2bc9e5303cf5f390fdea233d
ihebchatti <chatti.iheb93@gmail.com>
2020-02-11 09:35:40 +0100

    isAlpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,13 @@
 /**
 *_isalpha - checks for alphabetic characters
 *Return: 1 if lower/upper case && 0 otherwise
+*@c: integer
 */
 int _isalpha(int c)
 {
 	int c;
 
-	while ((c >= 'A' && c <= 'Z') || (c >= 'a' && c => 'z'))
+	if ((c > 'A' - 1 && c < 'Z' + 1) || (c > 'a' - 1 && c > 'z' + 1))
 	{
 		return (1);
 	}
