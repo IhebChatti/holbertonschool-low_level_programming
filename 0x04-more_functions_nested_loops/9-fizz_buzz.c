d7c5191c1f41d2289eea92c5bad6472e378e55b0
ihebchatti <chatti.iheb93@gmail.com>
2020-02-13 13:18:34 +0100

    fizzBuzzer

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,15 +8,19 @@ int main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
-	{
-		if ((i % 5 == 0) && (i % 3 == 0))
-			printf("FizzBuzz ");
-		else if (i % 5 == 0)
-			printf("Buzz ");
-		else if (i % 3 == 0)
-			printf("Fizz ");
-		else
-			printf("%d ", i);
+	{	if (i != 100)
+		{
+			if ((i % 5 == 0) && (i % 3 == 0))
+				printf("FizzBuzz ");
+			else if (i % 5 == 0)
+				printf("Buzz ");
+			else if (i % 3 == 0)
+				printf("Fizz ");
+			else
+				printf("%d ", i);
+		}
+		if (i == 100)
+			printf("Buzz");
 	}
 	printf("\n");
 	return (0);
