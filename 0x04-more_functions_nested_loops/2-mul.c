a11bd27d5f08300bc8d173cf03d3d7953f020e7f
ihebchatti <chatti.iheb93@gmail.com>
2020-02-13 10:41:34 +0100

    Mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+*_isdigit - function to multiply two digits
+*@a: int
+*@b: int
+*Return: 1 if digit and 0 if not
+*/
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
