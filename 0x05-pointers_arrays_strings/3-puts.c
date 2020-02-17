f8f162c05c2f7ab718cf2c4d6dd642c3b716a2ed
ihebchatti <chatti.iheb93@gmail.com>
2020-02-17 22:21:18 +0100

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+*_puts - a function that prints a string
+*@str: a pointer type char
+*Return: Void
+*/
+void _puts(char *str)
+{
+	while (*str)
+	{	
+		_putchar (*str);
+		str++;
+	}
+_putchar ('\n');
+}
