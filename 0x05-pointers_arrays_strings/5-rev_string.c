95a9e4d1817e78e7fc77fe11f77ebb2e20a1c713
ihebchatti <chatti.iheb93@gmail.com>
2020-02-18 00:57:32 +0100

    revString

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,12 +6,12 @@
 */
 void rev_string(char *s)
 {
-	int i, j;
+	int i, j, k;
 	char temp;
 
 	for (i = 0; s[i]; i++)
-		;
-	for (j = 0; j < i / 2; j++)
+		k = i;
+	for (j = 0; j < k / 2; j++)
 	{
 		temp = s[j];
 		s[j] = s[i - 1 - j];
