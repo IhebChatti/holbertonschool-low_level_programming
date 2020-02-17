aa0cbdb6394cb775b1b3fefd38fc5b4ad21704e0
ihebchatti <chatti.iheb93@gmail.com>
2020-02-17 23:09:11 +0100

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+*print_rev - a function to print a string in reverse
+*@s: an pointer type char
+*Return: Void
+*/
+void print_rev(char *s)
+{
+	int i = 0, j;
+
+	for (i = 0; s[i]; i++)
+		j = i;
+	for (i = j; i >= 0; i--)
+	_putchar (s[i]);
+	_putchar ('\n');
+}
