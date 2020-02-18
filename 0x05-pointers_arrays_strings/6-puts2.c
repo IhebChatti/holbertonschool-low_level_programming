c7f83afece42223fad7c6f2bccd9ee2176154b19
ihebchatti <chatti.iheb93@gmail.com>
2020-02-18 09:32:12 +0100

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+*puts2 - prints every character +2 from a string
+*@str: pointer type char
+*Return: void
+*/
+void puts2(char *str)
+{
+		while (*str)
+	{
+		_putchar (*str);
+		str += 2;
+	}
+_putchar ('\n');
+}
