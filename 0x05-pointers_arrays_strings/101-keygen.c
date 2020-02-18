4599019ab4315d1f891c6995d2998751d076126d
ihebchatti <chatti.iheb93@gmail.com>
2020-02-18 23:44:02 +0100

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,22 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/**
+*main - a program to generates a password
+*Return: 0
+*/
+int main(void)
+{
+	srand((unsignned int) (time(null)));
+	int i;
+	char pw[10];
+
+	for (i = 0; i <= 12; i++)
+	{
+		pw[i] = 33 + rand() % 94;	
+	}
+	pw[i] = '\0';
+	printf("%s\n", pw);
+	return (0);
+}
