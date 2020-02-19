ce0f8982f7e2068e8661bf84eb99382b84d604f8
ihebchatti <chatti.iheb93@gmail.com>
2020-02-19 12:22:00 +0100

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,10 +1,27 @@
 #include "holberton.h"
 /**
-*_atoi - convert a string to numb
-*@s: pointer type char
-*Return: 0
-*/
+ *_atoi - function to convert array to integer
+ *@s: pointer type char
+ *Return: converted int
+ */
 int _atoi(char *s)
 {
+	unsigned int result = 0;
+	int sign = 1;
+	int i = 0;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+			if (!(s[i + 1] >= '0' && s[i + 1] <= '9'))
+				break;
+		}
+	}
+	return (sign * result);
 }
