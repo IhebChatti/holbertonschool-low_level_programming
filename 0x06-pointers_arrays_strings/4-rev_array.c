6acf0a8357afc5b40e1154f802eca35f8eca4efa
ihebchatti <chatti.iheb93@gmail.com>
2020-02-20 14:13:51 +0100

    rev

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+*reverse_array - reverse an array
+*@a: pointer type int
+*@n: pointer type int
+*Return: void
+*/
+void reverse_array(int *a, int n)
+{
+	int j;
+	char temp;
+
+	for (j = 0; j <  2 && j < n; j++)
+	{
+		temp = a[j];
+		a[j] = a[n - 1 - j];
+		a[n - 1 - j + '\0'] = temp;
+}
+}
