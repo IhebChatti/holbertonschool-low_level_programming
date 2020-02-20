fb309fdf5694dabc0e213ecaf19af7939502837a
ihebchatti <chatti.iheb93@gmail.com>
2020-02-20 14:31:34 +0100

    revArray

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,17 +7,13 @@
 */
 void reverse_array(int *a, int n)
 {
-	int j, nn;
+	int j;
 	char temp;
 
-	if (n % 2 == 0)
-		nn = n + 1;
-	else
-		nn = n;
-	for (j = 0; j < (nn / 2); j++)
+	for (j = 0; j < (n / 2); j++)
 	{
 		temp = a[j];
-		a[j] = a[nn - 1 - j];
-		a[nn - 1 - j] = temp;
-}
+		a[j] = a[n - 1 - j];
+		a[n - 1 - j] = temp;
+	}
 }
