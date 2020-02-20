5e496dfaef280ff6db61e2b0fdd344639fca4ef7
ihebchatti <chatti.iheb93@gmail.com>
2020-02-20 13:45:37 +0100

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,24 +7,15 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	int x = 0;
-
-	while (x == 0)
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
 	{
-		if (s1[i] > s2[i])
-		{
-			x = 15;
-		}
-		else if (s1[i] < s2[i])
-		{
-			x = -15;
-		}
-		if (s1[i] == '\0')
-		{
-			break;
-		}
-		i++;
+		s1++;
+		s2++;
 	}
-	return (x);
+	if (*s1 == *s2)
+	{
+		return (0);
+	}
+	else
+		return (*s1 - *s2);
 }
