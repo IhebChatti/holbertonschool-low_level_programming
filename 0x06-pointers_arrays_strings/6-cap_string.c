262b029ec5e395b6e152a1243308dd95ce4bf7b5
ihebchatti <chatti.iheb93@gmail.com>
2020-02-20 18:05:39 +0100

    CAPITALIZEEEZEZEZE

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,7 +10,6 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-	
 		if (str[i] == ' ' || str[i] == '.'
 			|| str[i] == '\t' || str[i] == '\n'
 			|| str[i] == ';' || str[i] == '('
@@ -20,14 +19,14 @@ char *cap_string(char *str)
 			|| str[i] == '!')
 		{
 		if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-			{
-				str[i + 1] = str[i + 1] - 32;
-			}
+		{
+			str[i + 1] = str[i + 1] - 32;
+		}
 		}
 		else
 		{
-			if (str[i] <= 'A' && str[i] >= 'Z')
-				str[i] = str[i] + 32;
+			if (str[i + 1] <= 'A' && str[i + 1] >= 'Z')
+				str[i + 1] = str[i + 1] + 32;
 		}
 	}
 	return (str);
