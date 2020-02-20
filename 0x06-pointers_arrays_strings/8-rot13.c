e272d723b1a2e75d0d5ac8da61bc95231a0c7d7a
ihebchatti <chatti.iheb93@gmail.com>
2020-02-20 19:34:50 +0100

    rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -6,17 +6,21 @@
 */
 char *rot13(char *str)
 {
-	int i, j;
+	int i, j, x = 0;
 	char l1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char l2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; l1[j] != '\0'; j++)
+		for (j = 0; l1[j] != '\0' && x == 0; j++)
 		{
 			if (str[i] == l1[j])
+			{
 				str[i] = l2[j];
+				x = 1;
+			}
 		}
+		x = 0;
 	}
 	return (str);
 }
