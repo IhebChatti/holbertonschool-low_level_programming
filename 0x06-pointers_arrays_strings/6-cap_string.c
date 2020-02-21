817e5d436a952e0be18dc8c3a8e162346e348425
ihebchatti <chatti.iheb93@gmail.com>
2020-02-21 16:54:35 +0100

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,16 +19,9 @@ char *cap_string(char *str)
 			|| str[i] == '!')
 		{
 		if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-		{
 			str[i + 1] = str[i + 1] - 32;
 		}
-		}
-		else
-		{
-			if (str[i + 1] <= 'A' && str[i + 1] >= 'Z')
-				str[i + 1] = str[i + 1] - 32;
-		}
-		if ((str[0] >= 'a') && (str[0] <= 'z')
+		if ((str[0] >= 'a') && (str[0] <= 'z'))
 			str[0] = str[0] - 32;
 	}
 	return (str);
