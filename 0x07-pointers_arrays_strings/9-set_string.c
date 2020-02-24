8aa05eef64affa61b19a1c2bd85dab7263a1566e
ihebchatti <chatti.iheb93@gmail.com>
2020-02-24 21:55:52 +0100

    pointerToChar

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+*set_string - set value of pointer to char
+*@s: pointer to pointer type char
+*@to: pointer type char
+*Return: void
+*/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
