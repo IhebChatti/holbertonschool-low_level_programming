cb0e7329844bd6100dd9388b08585e0768af787d
ihebchatti <chatti.iheb93@gmail.com>
2020-02-29 11:36:35 +0100

    mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - Entry point
+*@argc: counter
+*@argv: vector
+*Return: 0 or 1 if error
+*/
+int main(int argc, char *argv[])
+{
+	int a, b, mul = 0;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		mul = a * b;
+		printf("%d\n", mul);
+	}
+	return (0);
+}
