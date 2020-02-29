1a11325c2582ebc207fc89bfe7215e1846512d99
ihebchatti <chatti.iheb93@gmail.com>
2020-02-29 12:16:30 +0100

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,17 +10,14 @@ int main(int argc, char *argv[])
 {
 	int a, b, mul = 0;
 
-	if (argc < 2)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		mul = a * b;
-		printf("%d\n", mul);
-	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	mul = a * b;
+	printf("%d\n", mul);
 	return (0);
 }
