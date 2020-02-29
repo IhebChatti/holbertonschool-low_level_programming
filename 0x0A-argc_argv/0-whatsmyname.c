1fcf8092370c1303f7a9d87b7c515d09776f094f
ihebchatti <chatti.iheb93@gmail.com>
2020-02-29 11:14:23 +0100

    saymyNAME

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
-int main(int argc, char* argv[])
+/**
+*main - Entry point
+*@argc: counter
+*@argv: vector
+*Return: int
+*/
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (argc);
