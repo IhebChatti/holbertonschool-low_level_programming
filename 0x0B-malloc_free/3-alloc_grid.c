41386e5efb6c4155d947dc0f78f798e4544f3abd
ihebchatti <chatti.iheb93@gmail.com>
2020-03-03 20:00:47 +0100

    allocate2D

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*alloc_grid - allocate a 2d array of ints
+*@width: width
+*@height: height
+*Return: pointer to 2d array
+*/
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i;
+
+	array = malloc(height * sizeof(int *));
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(width * sizeof(int));
+	}
+	return (array);
+	free(array);
+}
