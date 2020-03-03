531a42da6fbb371294e0fd2dbf2df29adbd55c90
ihebchatti <chatti.iheb93@gmail.com>
2020-03-03 20:11:54 +0100

    allocate2D

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -19,16 +19,16 @@ int **alloc_grid(int width, int height)
 	{
 		array[i] = malloc(width * sizeof(int));
 		if (array[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
 			{
-				for (j = 0; j < i; j++)
-				{
-					free(array[j]);
-					free(array);
-					return (NULL);
-				}
+				free(array[j]);
+				free(array);
+				return (NULL);
 			}
-			for (j = 0; j < width; j++)
-				array[i][j] = 0;
+		}
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
 	}
 	return (array);
 }
