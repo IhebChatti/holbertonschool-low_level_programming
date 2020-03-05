315490054e5a86d5fa44b3d89a01b9bd9ead9fe4
ihebchatti <chatti.iheb93@gmail.com>
2020-03-05 20:39:45 +0100

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -43,6 +43,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (ptr);
 	}
+	else if (new_size == 0 && ptr != '\0')
+	{
+		free(ptr);
+		return (NULL);
+
+	}
 	else
 	{
 		new_ptr = malloc(new_size);
