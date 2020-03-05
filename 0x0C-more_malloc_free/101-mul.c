f7bade905bec4ea16c9a408f784977bb1b44a6dc
ihebchatti <chatti.iheb93@gmail.com>
2020-03-05 20:57:47 +0100

    mul

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*
+*
+*
+*
+*/
+int main(int argc, char *argv[])
+{
+	unsigned int num1, num2, mul;
+	int i, j, l1, l2;
+
+	for (i = 0; i <= argc; i++)
+	{
+		for (j = 0; argv[1] != '\0'; j++)
+			l1++;
+		for (j =0; argv[2] != '\0'; j++)
+			l2++;
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	if (argc < 2)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	mul = num1 * num2;
+	printf("%u\n", mul);
+	return (0);
+}
