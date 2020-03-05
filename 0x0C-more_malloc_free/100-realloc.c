863abb065609b858603fa205a4939e1e190ce69d
ihebchatti <chatti.iheb93@gmail.com>
2020-03-05 12:52:27 +0100

    realloc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,57 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*_memcpy - function to copy memory area
+*@dest: destination pointer type char
+*@src: source pointer type char
+*@n: unsigned int
+*Return: dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *pDest = (char *)dest;
+	char *pSrc = (char *)src;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		pDest[i] = pSrc[i];
+	}
+	return (dest);
+}
+/**
+*_realloc - reallocates a mem block using malloc && free
+*@ptr: void pointer
+*@old_size: unsigned int
+*@new_size: unsigned int
+*Return: void pointer
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	else if (!ptr)
+	{
+		return (malloc(new_size));
+	}
+	else if (new_size <= old_size)
+	{
+		return (ptr);
+	}
+	else
+	{
+		new_ptr = malloc(new_size);
+		if (new_ptr)
+		{
+			_memcpy(new_ptr, ptr, old_size);
+			free(ptr);
+		}
+		return (new_ptr);
+	}
+}
