953a7f40f1a024c8379a204910771855d9af1058
ihebchatti <chatti.iheb93@gmail.com>
2020-03-05 11:29:17 +0100

    malloChecked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*malloc_checked - a function to allocate memory
+*@b: unsigned int
+*Return: void pointer
+*/
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b * sizeof(unsigned int));
+	if (ptr == NULL)
+		exit (98);
+	return (ptr);
+}
