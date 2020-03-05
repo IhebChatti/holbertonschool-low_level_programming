8753ce3ed8d7a36858770376bab9686d2a51a87a
ihebchatti <chatti.iheb93@gmail.com>
2020-03-05 12:15:33 +0100

    callocFix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,11 +10,14 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *arraymem;
+	unsigned int bzero;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	arraymem = malloc(nmemb * size);
 	if (arraymem == NULL)
 		return (NULL);
+	for (bzero = 0; bzero < (size * nmemb); bzero++)
+		arraymem[bzero] = '\0';
 	return (arraymem);
 }
