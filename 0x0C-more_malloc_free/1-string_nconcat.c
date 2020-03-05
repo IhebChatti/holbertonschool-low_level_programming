cd244137171d04bf21d451fa49509f4706014f90
ihebchatti <chatti.iheb93@gmail.com>
2020-03-05 20:00:23 +0100

    strnconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,11 +21,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == '\0')
 		s2 = "";
-	if (n >= l2)
-		n = l2;
 	str = malloc(l1 + n + 1);
 	if (str == NULL)
 		return (NULL);
+	if (n >= l2)
+		n = l2;
 	for (i = 0; s1[i] != '\0'; i++)
 		str[i] = s1[i];
 	for (i = 0; i < n; i++)
