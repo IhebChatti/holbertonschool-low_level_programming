55fdfa7db8b5b55da534b95c304948693b3d105c
ihebchatti <chatti.iheb93@gmail.com>
2020-03-09 22:20:05 +0100

    dogeUnleashed

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*free_dog - free memorty already allocated
+*@d: pointer type dog_t
+*Return: Void
+*/
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free (d);
+	}
+}
