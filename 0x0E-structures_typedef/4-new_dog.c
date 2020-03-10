ad325d22ba59e8ec0fb9386964676016857eed49
ihebchatti <chatti.iheb93@gmail.com>
2020-03-10 09:48:16 +0100

    newDoge

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+*_strdup - function that returns a pointer to newly allocated space in mem
+*@str: pointer type char
+*Return: pointer type char
+*/
+char *_strdup(char *str)
+{
+	int i = 0;
+	char *ing;
+	char *temp;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+		i++;
+	ing = malloc(i + 1);
+	temp = ing;
+	if (ing == NULL)
+		return (NULL);
+	while (*str)
+		*temp++ = *str++;
+	*temp = '\0';
+	return (ing);
+	free(ing);
+}
+/**
 *new_dog - creates a new dog
 *@name: pointer type char
 *@age: float
@@ -10,7 +35,10 @@
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *newdog;
+	dog_t *newdog;
+	char *nameCp;
+	char *ownerCp;
+	int lN, lOwn;
 
 	newdog = malloc(sizeof(struct dog));
 	if (newdog == NULL)
@@ -18,8 +46,23 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(newdog);
 		return (NULL);
 	}
-	(*newdog).name = name;
+	for (lN = 0; name[lN]; lN++)
+		;
+	for (lOwn = 0; owner[lOwn]; lOwn++)
+		;
+	nameCp = _strdup(name);
+	ownerCp = _strdup(owner);
+	nameCp = malloc(sizeof(lN));
+	ownerCp = malloc(sizeof(lOwn));
+	if (nameCp == NULL || ownerCp == NULL)
+	{
+		free(newdog);
+		free((*newdog).name);
+		free((*newdog).owner);
+		return (NULL);
+	}
+	(*newdog).name = nameCp;
 	(*newdog).age = age;
-	(*newdog).owner = owner;
+	(*newdog).owner = ownerCp;
 	return (newdog);
 }
