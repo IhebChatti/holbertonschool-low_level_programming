cbc257f70d87d48860852101efcb89e70f00945b
ihebchatti <chatti.iheb93@gmail.com>
2020-03-11 12:40:25 +0100

    arrayIterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,13 +11,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (size > 0 && array != NULL)
+	if (size > 0 && action != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
 			action(array[i]);
 		}
 	}
-	else 
-		return;
 }
