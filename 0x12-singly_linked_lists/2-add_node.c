c6bb8a120c3313f4fa04aad23365b4d6e5b5e1a3
ihebchatti <chatti.iheb93@gmail.com>
2020-03-21 11:19:07 +0100

    add node at the head

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,11 +1,27 @@
 #include "lists.h"
 
 /**
+ *_strlen - function that counts the lenght of a string
+ *@str: string to be counted
+ *
+ *Return: count
+ */
+
+int _strlen(const char *str)
+{
+	int i;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
  *add_node - function that adds a new node at the beginning of LL
  *@head: head of list
  *@str: string to be put at the head
  *
- *Return: 0 on success
+ *Return: pointer to new node
  */
 
 list_t *add_node(list_t **head, const char *str)
@@ -17,7 +33,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	new->next = *head;
 	new->str = strdup(str);
-	new->len = strlen(str);
+	new->len = _strlen(str);
 	*head = new;
 	return (new);
 }
