988a47d39a99734225fc126065030264b88243b6
ihebchatti <chatti.iheb93@gmail.com>
2020-03-23 01:01:12 +0100

    headerFile

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,29 @@
+#ifndef LISTS
+#define LISTS
+
+/* Includes */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/* Functions prototypes */
+
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+
+#endif
