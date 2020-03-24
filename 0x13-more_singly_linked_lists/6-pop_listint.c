4982ad0a4d7f724101f58893e650206b283415a9
ihebchatti <chatti.iheb93@gmail.com>
2020-03-24 10:52:28 +0100

    pop listint fix

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -15,8 +15,8 @@ int pop_listint(listint_t **head)
 	if (head == NULL)
 		return (0);
 	temp = *head;
-	*head = (*head)->next;
 	data = temp->n;
+	*head = (*head)->next;
 	free(temp);
 	return (data);
 }
