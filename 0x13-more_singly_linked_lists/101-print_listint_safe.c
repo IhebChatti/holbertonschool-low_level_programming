05d5cc8eb5e64f3d6881de957e7b95d71c99058c
ihebchatti <chatti.iheb93@gmail.com>
2020-03-25 12:07:35 +0100

    print listint_t safe mode

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,6 +12,8 @@ size_t print_listint_safe(const listint_t *head)
 	size_t i = 0;
 	const listint_t *temp;
 
+	if (head == NULL)
+		exit(98);
 	while (head != NULL)
 	{
 		printf("[%p] %d\n", (void *)head, head->n);
