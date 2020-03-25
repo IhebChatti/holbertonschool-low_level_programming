e51381f93d4a8fe2c277a65c9221c321668edef1
ihebchatti <chatti.iheb93@gmail.com>
2020-03-25 13:04:55 +0100

    detect loop in listint_t

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ *find_listint_loop - detects a loop in listint_t
+ *@head: head of the linked list
+ *
+ *Return: address of the node where loops starts on success
+ *NULL on failure
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow, *fast;
+
+	slow = head;
+	fast = head;
+	if (head == NULL)
+		return (NULL);
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next;
+		if (slow == fast)
+			return (slow);
+	}
+	return (slow);
+}
