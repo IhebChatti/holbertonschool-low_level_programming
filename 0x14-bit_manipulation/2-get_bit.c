2a90a6da8832bc2297ffad3c34db4fce6ce3250a
ihebchatti <chatti.iheb93@gmail.com>
2020-03-26 15:56:02 +0100

    get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,7 +10,13 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned long int i;
+
+	i = n & (1 << index);
 	if (index > sizeof(unsigned long int) * 8)
 		return (-1);
-	return ((n & (1 << index)) > 0);
+	if (i == 0)
+		return (0);
+	else
+		return (1);
 }
