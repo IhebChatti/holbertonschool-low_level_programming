bf5eb51a47d91949333c1bc0164753e9e3467c66
ihebchatti <chatti.iheb93@gmail.com>
2020-03-26 14:35:06 +0100

    header File

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -4,12 +4,13 @@
 /* Include libraries */
 
 #include <stdio.h>
-#include <string.h>
 
 /* Functions prototypes */
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
-
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 
 #endif
