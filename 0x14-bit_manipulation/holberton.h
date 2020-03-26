080dfa44321fa8a0589c1f71701bac96719cc21d
ihebchatti <chatti.iheb93@gmail.com>
2020-03-26 13:34:29 +0100

    header File

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,15 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/* Include libraries */
+
+#include <stdio.h>
+#include <string.h>
+
+/* Functions prototypes */
+
+unsigned int binary_to_uint(const char *b);
+
+
+
+#endif
