91ff00b584e47a7d58e5ac072d0df8b7ff117e8f
ihebchatti <chatti.iheb93@gmail.com>
2020-03-31 17:48:57 +0100

    copy from file to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,10 +16,8 @@ int main(int argc, char **argv)
 	char buff[BUFFERSIZE];
 
 	if (argc != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+
 	if (argv[1] == NULL)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 
@@ -30,18 +28,12 @@ int main(int argc, char **argv)
 	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	fr = read(file_from, buff, BUFFERSIZE);
 	if (fr  == -1)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
 	while (fr != 0)
 	{
 		fw = write(file_to, buff, fr);
 		if (fw == -1)
-		{
 			dprintf(STDERR_FILENO, " Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
 		fr = read(file_from, buff, BUFFERSIZE);
 	}
 	fw = close(file_from);
