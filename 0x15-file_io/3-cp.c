8564ca06319e458f808223e698648f53caff87e6
ihebchatti <chatti.iheb93@gmail.com>
2020-03-31 19:27:39 +0100

    cp from file to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,14 +17,14 @@ int main(int argc, char **argv)
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
 		exit(97);
-	file_from = open(argv[1], O_RDONLY);
-	if (file_from == -1)
+	if (argv[1] == NULL)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 		exit(98);
-	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-	if (file_to == -1)
+	if (argv[2] == NULL)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]),
 		exit(99);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	readvalue = read(file_from, buffer, BUFFER_SIZE);
 	if (readvalue == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
