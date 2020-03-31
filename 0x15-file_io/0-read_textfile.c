c70077e95be3b9a11efadbcbb6e62dd0a0844219
ihebchatti <chatti.iheb93@gmail.com>
2020-03-31 14:28:42 +0100

    she can hear

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	fr = read(fd, temp, letters);
 	fw = write(1, temp, fr);
-	if (fw < 0 || fd == -1)
+	if (fw == -1 || fd == -1)
 		return (0);
 	close(fd);
 	free(temp);
