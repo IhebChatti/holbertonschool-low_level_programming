1400274e8481a46cc94f9911046895f193864c8c
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-02 02:56:24 +0100

    insert node at index DLL

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,7 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new == NULL)
 		return (NULL);
 	if (temp->next == NULL)
-		return (add_dnodeint(h, n));
+		return (add_dnodeint_end(h, n));
 	new->n = n;
 	temp->next->prev = new;
 	new->prev = temp;
