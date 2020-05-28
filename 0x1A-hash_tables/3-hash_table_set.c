60aa4d5017357b5265b9976430e7d95156f962a7
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-28 22:44:54 +0100

    hashtable set by key/value

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int idx;
+	hash_node_t *new;
+
+	if (!ht || !key || !*key || !value)
+		return (0);
+	new = malloc(sizeof(hash_node_t));
+	if (!new)
+		return (0);
+	idx = key_index((const unsigned char *)key, ht->size);
+	while (ht->array[idx])
+	{
+		if (strcmp(ht->array[idx]->key, key) == 0)
+		{
+			free(ht->array[idx]->value);
+			ht->array[idx]->value = strdup(value);
+			return (1);
+		}
+		idx++;
+	}
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
+	return (1);
+}
