77f2fcfcd6596cf97d380ed11d275927dfc183d6
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-29 11:22:07 +0100

    added prototype

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get the values by key
+ * @ht: hashtable
+ * @key: the key we searching
+ *
+ * Return: Value on success, NULL on failure
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i;
+	hash_node_t *pair;
+
+	i = key_index((const unsigned char *)key, ht->size);
+	pair = ht->array[i];
+	while (pair && pair->key && strcmp(key, pair->key) > 0)
+		pair = pair->next;
+
+	if (!pair || !pair->key || strcmp(key, pair->key) < 0)
+		return (NULL);
+	return (pair->value);
+}
