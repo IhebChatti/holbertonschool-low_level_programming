d230204f5eb78fa4caedcc8cc30e74c725a26fc3
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-29 15:07:19 +0100

    print table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,25 +9,18 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i = 0;
-	hash_node_t *temp;
 	int flag = 0;
 
-	if (!ht)
+	if (!ht || !ht->array)
 		return;
 	printf("{");
 	while (i < ht->size)
 	{
-		temp = ht->array[i];
-		if (temp)
+		if (ht->array[i])
 		{
 			if (flag == 1)
 				printf(", ");
-			while (temp->next)
-			{
-				printf("'%s': '%s'", temp->key, temp->value);
-				temp = temp->next;
-			}
-			printf("'%s': '%s'", temp->key, temp->value);
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
 			flag = 1;
 		}
 		i++;
