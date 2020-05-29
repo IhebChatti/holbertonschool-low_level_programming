d2a68a09bc53ea8080ad5670724f74ca19b80a04
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-29 11:51:52 +0100

    display hashtable

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - display the hashtable
+ * @ht: hashtable to display
+ *
+ * Return: Void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	int flag = 0;
+
+	printf("{");
+	while (i < ht->size)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			flag = 1;
+		}
+		i++;
+	}
+	printf("}\n");
+}
