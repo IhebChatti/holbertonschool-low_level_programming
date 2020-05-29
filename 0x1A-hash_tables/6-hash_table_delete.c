674250bf0134dcb1d5cb711b365659aceeda1a31
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-29 12:34:45 +0100

    delete hashtable

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -11,16 +11,20 @@ void hash_table_delete(hash_table_t *ht)
 	unsigned long int i = 0;
 	hash_node_t *temp;
 
+	if (!ht)
+		return;
 	while (i < ht->size)
 	{
-		temp = ht->array[i];
-		while (temp)
-		{
-			free(temp->key);
-			free(temp->value);
-			temp = temp->next;
-		}
+			temp = ht->array[i];
+			while (temp)
+			{
+				free(temp->key);
+				free(temp->value);
+				temp = temp->next;
+			}
+			free(temp);
 		i++;
 	}
+	free(ht->array);
 	free(ht);
 }
