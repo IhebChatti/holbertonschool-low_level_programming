8114347536a01e182de8016f145c241ded1ead75
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-29 11:02:49 +0100

    create hashtable

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,7 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	ht->size = size;
 	ht->array = malloc(size * sizeof(hash_node_t));
-	if (!ht->array)
+	if (ht->array == NULL)
 	{
 		free(ht);
 		return (NULL);
