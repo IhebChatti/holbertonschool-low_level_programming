2969964ae0e54fa1d90a002d06355f906935e0c9
IhebChatti <iheb.chatti@holbertonschool.com>
2020-05-29 14:58:44 +0100

    set ht

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,23 +11,25 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int idx;
-	hash_node_t *new = NULL;
+	hash_node_t *new = NULL, *temp;
 
 	if (!ht || !key || !*key || !value)
 		return (0);
 
 	idx = key_index((const unsigned char *)key, ht->size);
-	while (ht->array[idx])
+	temp = ht->array[idx];
+	if (temp)
 	{
-		if (strcmp(ht->array[idx]->key, key) == 0)
+		while (temp && strcmp(temp->key, key) != 0)
+			temp = temp->next;
+		if (temp && strcmp(temp->key, key) == 0)
 		{
-			free(ht->array[idx]->value);
-			ht->array[idx]->value = strdup(value);
-			if (!ht->array[idx]->value)
+			free(temp->value);
+			temp->value = strdup(value);
+			if (!temp->value)
 				return (0);
 			return (1);
 		}
-		idx++;
 	}
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
